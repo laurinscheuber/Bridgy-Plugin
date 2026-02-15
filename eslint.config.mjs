79a928a14213caaf8a754ba90372bbb2723e94c9
laurinscheuber <38795964+laurinscheuber@users.noreply.github.com>
2026-02-15 11:54:40 +0100

    chore(lint): configure ESLint

---
+++ eslint.config.mjs
@@ -0,0 +1,22 @@
+import globals from "globals";
+import pluginJs from "@eslint/js";
+import tseslint from "typescript-eslint";
+
+
+/** @type {import('eslint').Linter.Config[]} */
+export default [
+  {files: ["**/*.{js,mjs,cjs,ts}"]},
+  {languageOptions: { globals: globals.browser }},
+  pluginJs.configs.recommended,
+  ...tseslint.configs.recommended,
+  {
+    rules: {
+      "@typescript-eslint/no-explicit-any": "warn",
+      "@typescript-eslint/no-unused-vars": "warn",
+      "no-console": ["warn", { allow: ["warn", "error"] }]
+    }
+  },
+  {
+    ignores: ["dist/**", "code.js", "ui.html", "scripts/**", "build.js"]
+  }
+];
